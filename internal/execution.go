2dd01f4c7d0a1ece7c44aad90087a112d152fdc3
Nick Pillitteri <nick.pillitteri@grafana.com>
2023-09-05 11:50:39 -0400

    Unused import alias
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- internal/execution.go
+++ internal/execution.go
@@ -1,6 +1,6 @@
 package internal
 
-import failsafe "github.com/failsafe-go/failsafe-go"
+import "github.com/failsafe-go/failsafe-go"
 
 func NewExecutionAttempt[R any](result *failsafe.ExecutionResult[R], exec *failsafe.Execution[R]) failsafe.ExecutionAttempt[R] {
 	return failsafe.ExecutionAttempt[R]{
