2410329176ef2a061c72f5724e6fb43096882d65
Jonathan Halterman <jhalterman@gmail.com>
2024-11-21 17:04:35 -0800

    wip

---
+++ adaptivelimiter/adaptivelimiter.go
@@ -0,0 +1,350 @@
+package adaptivelimiter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
+
+	"github.com/failsafe-go/failsafe-go"
+	"github.com/failsafe-go/failsafe-go/internal/util"
+	"github.com/failsafe-go/failsafe-go/policy"
+)
+
+// ErrExceeded is returned when an execution exceeds the current limit.
+var ErrExceeded = errors.New("limit exceeded")
+
+const warmupSamples = 10
+
+// AdaptiveLimiter is a concurrency limiter that adjusts its limit up or down based on latency trends:
+//  - When recent latencies are trending up relative to longer term latencies, the concurrency limit is decreased.
+//  - When recent latencies are trending down relative to longer term latencies, the concurrency limit is increased.
+//
+// To accomplish this, recent average latencies are tracked and regularly compared to a weighted moving average of
+// longer term latencies. Limit increases are additionally controlled to ensure they don't increase latency. Any
+// executions in excess of the limit will be rejected with ErrExceeded.
+//
+// By default, an AdaptiveLimiter will converge on a concurrency limit that represents the capacity of the machine it's
+// running on, and avoids having executions queue up. Since running a limit without allowing for queueing is too strict
+// in some cases and may cause unexpected rejections, optional blocking of requests when the limiter is full can be
+// enabled by configuring a maxLatency.
+//
+// When blocking is enabled and the limiter is full, requests block up to some max latency based on an estimated latency
+// for incoming requests. Estimated latency considers the current number of blocked requests, the current limit, and the
+// average request processing time.
+//
+// R is the execution result type. This type is concurrency safe.
+type AdaptiveLimiter[R any] interface {
+	failsafe.Policy[R]
+
+	// AcquirePermit attempts to acquire a permit to perform an execution via the limiter, waiting until one is
+	// available or the execution is canceled. Returns [context.Canceled] if the ctx is canceled.
+	// Callers must call Record or Drop to release a successfully acquired permit back to the limiter.
+	// ctx may be nil.
+	AcquirePermit(context.Context) (Permit, error)
+
+	// TryAcquirePermit attempts to acquire a permit to perform an execution via the limiter, returning whether the
+	// Permit was acquired or not. Callers must call Record or Drop to release a successfully acquired permit back
+	// to the limiter.
+	TryAcquirePermit() (Permit, bool)
+
+	// Limit returns the concurrent execution limit, as calculated by the adaptive limiter.
+	Limit() int
+
+	// Inflight returns the current number of inflight executions.
+	Inflight() int
+
+	// Blocked returns the current number of blocked executions.
+	Blocked() int
+}
+
+// Permit is a permit to perform an execution that must be completed by calling Record or Drop.
+type Permit interface {
+	// Record records an execution completion and releases a permit back to the limiter. The execution duration will be used
+	// to influence the limiter.
+	Record()
+
+	// Drop releases an execution permit back to the limiter without recording a completion. This should be used when an
+	// execution completes prematurely, such as via a timeout, and we don't want the execution duration to influence the
+	// limiter.
+	Drop()
+}
+
+/*
+Builder builds AdaptiveLimiter instances.
+
+This type is not concurrency safe.
+*/
+type Builder[R any] interface {
+	WithShortWindow(minDuration time.Duration, maxDuration time.Duration, minWindowSize uint) Builder[R]
+
+	WithLongWindow(longWindowSize uint) Builder[R]
+
+	WithLimits(minLimit uint, maxLimit uint, initialLimit uint) Builder[R]
+
+	WithCovarianceWindow(covarianceWindowSize uint) Builder[R]
+
+	WithSmoothing(smoothing float64) Builder[R]
+
+	WithMaxLatency(maxLatency time.Duration) Builder[R]
+
+	WithLogger(logger *slog.Logger) Builder[R]
+
+	OnLimitChanged(listener func(event LimitChangedEvent)) Builder[R]
+
+	// Build returns a new AdaptiveLimiter using the builder's configuration.
+	Build() AdaptiveLimiter[R]
+}
+
+// LimitChangedEvent indicates an AdaptiveLimiter's limit has changed.
+type LimitChangedEvent struct {
+	OldLimit uint
+	NewLimit uint
+}
+
+type config[R any] struct {
+	logger            *slog.Logger
+	minWindowDuration time.Duration
+	maxWindowDuration time.Duration
+	minWindowSamples  uint
+	longWindow        uint
+	covarianceWindow  uint
+
+	initialLimit uint
+	minLimit     float64
+	maxLimit     float64
+	smoothing    float64
+
+	maxLatency           time.Duration
+	limitChangedListener func(LimitChangedEvent)
+}
+
+var _ Builder[any] = &config[any]{}
+
+func NewBuilder[R any]() Builder[R] {
+	return &config[R]{
+		minWindowDuration: time.Second,
+		maxWindowDuration: time.Second,
+		minWindowSamples:  1,
+		longWindow:        60,
+		covarianceWindow:  20,
+		initialLimit:      20,
+		minLimit:          1,
+		maxLimit:          200,
+		smoothing:         0.2,
+	}
+}
+
+func (c *config[R]) WithShortWindow(minDuration time.Duration, maxDuration time.Duration, minWindowSize uint) Builder[R] {
+	c.minWindowDuration = minDuration
+	c.maxWindowDuration = maxDuration
+	c.minWindowSamples = minWindowSize
+	return c
+}
+
+func (c *config[R]) WithLongWindow(longWindowSize uint) Builder[R] {
+	c.longWindow = longWindowSize
+	return c
+}
+
+func (c *config[R]) WithLimits(minLimit uint, maxLimit uint, initialLimit uint) Builder[R] {
+	c.minLimit = float64(minLimit)
+	c.maxLimit = float64(maxLimit)
+	c.initialLimit = initialLimit
+	return c
+}
+
+func (c *config[R]) WithCovarianceWindow(covarianceWindowSize uint) Builder[R] {
+	c.covarianceWindow = covarianceWindowSize
+	return c
+}
+
+func (c *config[R]) WithSmoothing(smoothing float64) Builder[R] {
+	c.smoothing = smoothing
+	return c
+}
+
+func (c *config[R]) WithMaxLatency(maxLatency time.Duration) Builder[R] {
+	c.maxLatency = maxLatency
+	return c
+}
+
+func (c *config[R]) WithLogger(logger *slog.Logger) Builder[R] {
+	c.logger = logger
+	return c
+}
+
+func (c *config[R]) OnLimitChanged(listener func(event LimitChangedEvent)) Builder[R] {
+	c.limitChangedListener = listener
+	return c
+}
+
+func (c *config[R]) Build() AdaptiveLimiter[R] {
+	var result AdaptiveLimiter[R]
+	adaptive := &adaptiveLimiter[R]{
+		config:     c,
+		semaphore:  util.NewDynamicSemaphore(int64(c.initialLimit)),
+		limit:      float64(c.initialLimit),
+		window:     newSampleWindow(),
+		longRTT:    util.NewEWMA(c.longWindow, warmupSamples),
+		covariance: newCovarianceWindow(c.covarianceWindow),
+	}
+	if c.maxLatency != 0 {
+		result = &blockingLimiter[R]{
+			adaptiveLimiter: adaptive,
+		}
+	} else {
+		result = adaptive
+	}
+	return result
+}
+
+type adaptiveLimiter[R any] struct {
+	*config[R]
+
+	// Mutable state
+	semaphore *util.DynamicSemaphore
+	mu        sync.Mutex
+
+	// Guarded by mu
+	limit          float64            // The current concurrency limit
+	window         *sampleWindow      // Tracks short term average latency
+	nextUpdateTime time.Time          // Tracks when the limit can next be updated
+	longRTT        util.MovingAverage // Tracks long term average latency
+	covariance     *covarianceWindow  // Tracks the correlation between concurrency latency
+}
+
+func (l *adaptiveLimiter[R]) AcquirePermit(ctx context.Context) (Permit, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	if err := l.semaphore.Acquire(ctx); err != nil {
+		return nil, err
+	}
+	return &recordingPermit[R]{
+		limiter:         l,
+		startTime:       time.Now(),
+		currentInflight: l.semaphore.Inflight(),
+	}, nil
+}
+
+func (l *adaptiveLimiter[R]) TryAcquirePermit() (Permit, bool) {
+	if !l.semaphore.TryAcquire() {
+		return nil, false
+	}
+	return &recordingPermit[R]{
+		limiter:         l,
+		startTime:       time.Now(),
+		currentInflight: l.semaphore.Inflight(),
+	}, true
+}
+
+func (l *adaptiveLimiter[R]) Limit() int {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return int(l.limit)
+}
+
+func (l *adaptiveLimiter[R]) Inflight() int {
+	return l.semaphore.Inflight()
+}
+
+func (l *adaptiveLimiter[R]) Blocked() int {
+	return 0
+}
+
+// Records the round-trip time of a completed execution, updating the concurrency limit if the short window is full.
+func (l *adaptiveLimiter[R]) record(startTime time.Time, inflight int, dropped bool) {
+	l.semaphore.Release()
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	now := time.Now()
+	rtt := now.Sub(startTime)
+	if !dropped {
+		l.window = l.window.AddSample(rtt)
+	}
+
+	if now.After(l.nextUpdateTime) && l.window.count >= l.minWindowSamples {
+		l.updateLimit(l.window.AverageRTT(), inflight)
+		l.window = newSampleWindow()
+		minWindowTime := max(l.window.minRTT*2, l.minWindowDuration)
+		l.nextUpdateTime = now.Add(min(minWindowTime, l.maxWindowDuration))
+	}
+}
+
+func (l *adaptiveLimiter[R]) updateLimit(rtt time.Duration, inflight int) {
+	shortRTT := float64(rtt)
+	longRTT := l.longRTT.Add(float64(rtt))
+
+	// Compute the gradient as the rate of change between the long term and short term latencies
+	gradient := longRTT / shortRTT
+
+	// Adjust the gradient based on any covariance between concurrency and latency.
+	// Covariance indicates whether increases in recent limits correlate to increases in recent latencies.
+	// This is necessary to avoid situations where the gradient and limit rise indefinitely.
+	covariance := l.covariance.Add(float64(inflight), shortRTT)
+	if covariance > 0 {
+		// Decrease if concurrency correlates with higher latency
+		gradient = gradient * 0.9
+		covariance = 1
+	} else if covariance < 0 {
+		// Increase if concurrency does not correlate with higher latency
+		gradient = gradient * 1.1
+		covariance = -1
+	}
+
+	// Clamp the gradient
+	gradient = max(0.5, min(1.5, gradient))
+
+	// Adjust, smooth, and clamp the limit based on the gradient
+	newLimit := l.limit * gradient
+	newLimit = util.Smooth(l.limit, newLimit, l.smoothing)
+	newLimit = max(l.minLimit, min(l.maxLimit, newLimit))
+
+	// Clamp increases to limit relative to concurrency
+	if newLimit > float64(inflight)*10 {
+		return
+	}
+
+	if l.logger != nil && l.logger.Enabled(nil, slog.LevelDebug) {
+		l.logger.Debug(fmt.Sprintf("newLimit=%0.2f, oldLimit=%0.2f, inflight=%d, shortRTT=%0.2f, longRTT=%0.2f, covariance=%d, gradient=%0.2f", newLimit, l.limit, inflight, shortRTT/1e6, longRTT/1e6, int(covariance), gradient))
+	}
+
+	if uint(l.limit) != uint(newLimit) {
+		if l.limitChangedListener != nil {
+			l.limitChangedListener(LimitChangedEvent{
+				OldLimit: uint(l.limit),
+				NewLimit: uint(newLimit),
+			})
+		}
+	}
+
+	l.semaphore.SetSize(int64(newLimit))
+	l.limit = newLimit
+}
+
+func (l *adaptiveLimiter[R]) ToExecutor(_ R) any {
+	e := &adaptiveExecutor[R]{
+		BaseExecutor:    &policy.BaseExecutor[R]{},
+		adaptiveLimiter: l,
+	}
+	e.Executor = e
+	return e
+}
+
+type recordingPermit[R any] struct {
+	limiter         *adaptiveLimiter[R]
+	startTime       time.Time
+	currentInflight int
+}
+
+func (p *recordingPermit[R]) Record() {
+	p.limiter.record(p.startTime, p.currentInflight, false)
+}
+
+func (p *recordingPermit[R]) Drop() {
+	p.limiter.record(p.startTime, p.currentInflight, true)
+}
---
+++ adaptivelimiter/blockinglimiter.go
@@ -0,0 +1,73 @@
+package adaptivelimiter
+
+import (
+	"context"
+	"time"
+)
+
+// blockingLimiter wraps an AdaptiveLimiter and blocks some portion of requests when the AdaptiveLimiter is at its
+// limit.
+type blockingLimiter[R any] struct {
+	*adaptiveLimiter[R]
+
+	// Guarded by mu
+	blockedCount int
+}
+
+func (l *blockingLimiter[R]) AcquirePermit(ctx context.Context) (Permit, error) {
+	// Try to get a permit without waiting
+	if permit, ok := l.TryAcquirePermit(); ok {
+		return permit, nil
+	}
+
+	// Estimate if the blocking limiter is full
+	l.mu.Lock()
+	estimatedLatency := l.estimateLatency()
+	if estimatedLatency > l.maxLatency {
+		l.mu.Unlock()
+		return nil, ErrExceeded
+	}
+
+	// Acquire a permit, blocking if needed
+	l.blockedCount++
+	l.mu.Unlock()
+	permit, err := l.adaptiveLimiter.AcquirePermit(ctx)
+	l.mu.Lock()
+	l.blockedCount--
+	l.mu.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return permit, nil
+}
+
+// estimateLatency estimates the latency for a new request by considering the delegate's limit, how many batches of
+// blocked requests it would take before a new request is serviced, and the average processing time per request.
+func (l *blockingLimiter[R]) estimateLatency() time.Duration {
+	avgProcessing := time.Duration(l.longRTT.Value())
+	if avgProcessing == 0 {
+		avgProcessing = l.maxLatency / warmupSamples
+	}
+
+	// Include current request in the latency estimate
+	totalRequests := l.blockedCount + 1
+
+	// Calculate complete batches needed
+	concurrency := l.Limit()
+	fullBatches := totalRequests / concurrency
+
+	// If we have any remaining requests, count it as a full batch
+	if totalRequests%concurrency > 0 {
+		fullBatches++
+	}
+
+	return time.Duration(float64(fullBatches) * float64(avgProcessing))
+}
+
+func (l *blockingLimiter[R]) Blocked() int {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.blockedCount
+}
