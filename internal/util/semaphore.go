28dfbbfdd64461794d9058e9bff04f3e1255a281
Jonathan Halterman <jhalterman@gmail.com>
2025-02-12 18:04:25 -0800

    Update dynamic sempahore impl

--- internal/util/semaphore.go
+++ internal/util/semaphore.go
@@ -1,96 +1,59 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package util
 
 import (
 	"container/list"
 	"context"
 	"sync"
-	"time"
 )
 
-// DynamicSemaphore implements a semaphore whose capacity can be changed dynamically at
-// run time.
+// DynamicSemaphore is a semaphore that can be dynamically resized and allows FIFO blocking when full.
 type DynamicSemaphore struct {
-	mu           sync.Mutex
-	size         int64
-	used         int64
-	waiters      list.List
-	blockedSince time.Time
+	mu      sync.Mutex
+	size    int       // Guarded by mu
+	used    int       // Guarded by mu
+	waiters list.List // Guarded by mu
 }
 
-// NewDynamicSemaphore returns a dynamic semaphore with the given initial capacity. Note
-// that this is for convenience and to match golang.org/x/sync/semaphore however
-// it's possible to use a zero-value semaphore provided SetSize is called before
-// use.
-func NewDynamicSemaphore(n int64) *DynamicSemaphore {
-	return &DynamicSemaphore{size: n}
+func NewDynamicSemaphore(size int) *DynamicSemaphore {
+	return &DynamicSemaphore{size: size}
 }
 
-// SetSize dynamically updates the number of available slots. If there are more
-// than n slots currently acquired, no further acquires will succeed until
-// sufficient have been released to take the total outstanding below n again.
-func (s *DynamicSemaphore) SetSize(n int64) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	// If capacity increased, wake up waiters that can now acquire
-	if n > s.size {
-		for s.used < n && s.waiters.Len() > 0 {
-			s.used++
-			next := s.waiters.Front()
-			s.waiters.Remove(next)
-			close(next.Value.(chan struct{}))
-		}
-	}
-
-	s.size = n
-}
-
-// Acquire attempts to acquire one "slot" in the semaphore, blocking only until
-// ctx is Done. On success, returns nil. On failure, returns ctx.Err() and leaves
-// the semaphore unchanged.
-//
-// If ctx is already done, Acquire may still succeed without blocking.
+// Acquire acquires a permit from the sempahore, blocking until one is made available via Release or the ctx is Done.
+// Blocking callers are unblocked in FIFO order as permits are released.
 func (s *DynamicSemaphore) Acquire(ctx context.Context) error {
 	s.mu.Lock()
+
+	// See if a permit is immediately available
 	if s.used < s.size {
 		s.used++
 		s.mu.Unlock()
 		return nil
 	}
 
-	// Need to wait, Add to waiter list
-	ready := make(chan struct{})
-	if s.waiters.Len() == 0 {
-		s.blockedSince = time.Now()
-	}
-	elem := s.waiters.PushBack(ready)
+	// Create a waiter for a permit
+	waiter := make(chan struct{})
+	waiterElem := s.waiters.PushBack(waiter)
 	s.mu.Unlock()
 
 	select {
 	case <-ctx.Done():
-		err := ctx.Err()
 		s.mu.Lock()
+		defer s.mu.Unlock()
+		// Remove waiter if it wasn't fulfilled
 		select {
-		case <-ready:
-			// Acquired the semaphore after we were canceled.  Rather than trying to
-			// fix up the queue, just pretend we didn't notice the cancellation.
-			err = nil
+		case <-waiter:
+			return nil
 		default:
-			s.waiters.Remove(elem)
+			s.waiters.Remove(waiterElem)
+			return ctx.Err()
 		}
-		s.mu.Unlock()
-		return err
 
-	case <-ready:
+	case <-waiter:
 		return nil
 	}
 }
 
-// TryAcquire attempts to acquire one "slot" in the semaphore without blocking.
-// Returns true if successful, else false if no slots are immediately available.
+// TryAcquire acquires a permit if one is available, returning true, else returns false if no permits are available.
 func (s *DynamicSemaphore) TryAcquire() bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -102,38 +65,46 @@ func (s *DynamicSemaphore) TryAcquire() bool {
 	return false
 }
 
-// Release releases the semaphore. It will panic if release is called on an
-// empty semphore.
+// Release releases a permit which unblocks a waiter if one exists.
+// Panics if called without a corresponding call to Acquire or TryAcqyure.
 func (s *DynamicSemaphore) Release() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if s.used < 1 {
-		panic("DynamicSemaphore: unexpected release")
+	// Check for invalid state
+	if s.used <= 0 {
+		panic("DynamicSemaphore: Release called without matching Acquire")
 	}
 
-	waiter := s.waiters.Front()
+	s.used--
 
-	// If there are no waiters, or if we recently resized and used is too high, just decrement and we're done
-	if waiter == nil || s.used > s.size {
-		s.used--
-		return
+	// If we have waiters and capacity, wake up the next waiter
+	if s.used < s.size && s.waiters.Len() > 0 {
+		s.wakeAndAcquire(s.waiters.Front())
 	}
-
-	s.waiters.Remove(waiter)
-	if s.waiters.Len() == 0 {
-		s.blockedSince = time.Time{}
-	}
-
-	// Release the waiter
-	close(waiter.Value.(chan struct{}))
 }
 
-// BlockedSince returns the time since the semaphore has been blocked, if any.
-func (s *DynamicSemaphore) BlockedSince() time.Time {
+// SetSize resizes the semaphore. If the size is increased, waiters will be unblocked as needed.
+func (s *DynamicSemaphore) SetSize(size int) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	return s.blockedSince
+
+	// If capacity increased, wake up waiters that can now acquire
+	if size > s.size {
+		for s.used < size && s.waiters.Len() > 0 {
+			s.wakeAndAcquire(s.waiters.Front())
+		}
+	}
+
+	s.size = size
+}
+
+// Wakes a blocked waiter and acquires a permit.
+// Requires s.mu to be locked before calling.
+func (s *DynamicSemaphore) wakeAndAcquire(waiterElem *list.Element) {
+	s.used++
+	s.waiters.Remove(waiterElem)
+	close(waiterElem.Value.(chan struct{}))
 }
 
 func (s *DynamicSemaphore) IsFull() bool {
@@ -149,9 +120,9 @@ func (s *DynamicSemaphore) Waiters() int {
 	return s.waiters.Len()
 }
 
-// Used returns how many slots are currently in use.
+// Used returns how many permits are currently in use.
 func (s *DynamicSemaphore) Used() int {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	return int(s.used)
+	return s.used
 }
