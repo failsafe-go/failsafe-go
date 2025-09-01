38bce2c4aea00f4d435efa8ae195ecb8b8e56a44
Jonathan Halterman <jhalterman@gmail.com>
2025-08-31 22:07:04 -0700

    Improve prioritizer test coverage

--- adaptivelimiter/prioritizer_test.go
+++ adaptivelimiter/prioritizer_test.go
@@ -1,7 +1,9 @@
 package adaptivelimiter
 
 import (
+	"bytes"
 	"context"
+	"log/slog"
 	"testing"
 	"time"
 
@@ -38,4 +40,52 @@ func TestPrioritizer_Calibrate(t *testing.T) {
 	p.Calibrate()
 	assert.Equal(t, .5, p.RejectionRate())
 	assert.True(t, p.rejectionThreshold.Load() > 0 && p.rejectionThreshold.Load() < 200, "low priority execution should be rejected")
+}
+
+func TestPrioritizer_WithLogger(t *testing.T) {
+	// Given
+	var buf bytes.Buffer
+	logger := slog.New(slog.NewTextHandler(&buf, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	p := NewPrioritizerBuilder().WithLogger(logger).Build()
+
+	// When
+	p.Calibrate()
+
+	// Then
+	assert.Contains(t, buf.String(), "prioritizer calibration")
+}
+
+func TestPrioritizer_ScheduleCalibrations(t *testing.T) {
+	// Given
+	p := NewPrioritizer()
+	limiter := NewBuilder[any]().
+		WithLimits(1, 1, 1).
+		WithQueueing(1, 2).
+		BuildPrioritized(p)
+	shouldAcquireWithPriority(t, limiter, priority.Low)    // fill the limiter
+	go shouldAcquireWithPriority(t, limiter, priority.Low) // fill the queue
+	go shouldAcquireWithPriority(t, limiter, priority.Low) // fill the queue
+	assertQueued(t, limiter, 2)
+
+	// When
+	cancel := p.ScheduleCalibrations(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	// Wait for calibration
+	time.Sleep(50 * time.Millisecond)
+
+	// Then
+	assert.Greater(t, p.RejectionRate(), 0.0)
+	assert.GreaterOrEqual(t, p.RejectionThreshold(), 100)
+	assert.LessOrEqual(t, p.RejectionThreshold(), 199)
+}
+
+func TestPrioritizer_Register(t *testing.T) {
+	p := NewPrioritizer().(*prioritizer)
+	assert.Len(t, p.statsFuncs, 0)
+	p.register(func() (int, int, int, int) {
+		return 0, 0, 0, 0
+	})
+	assert.Len(t, p.statsFuncs, 1)
 }
