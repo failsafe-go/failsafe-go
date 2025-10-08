97881b84e37733f83cc5875f39b505348de0343c
Jonathan Halterman <jhalterman@gmail.com>
2025-10-07 22:37:40 -0700

    Add windowed level tracking

--- priority/priority_test.go
+++ priority/priority_test.go
@@ -2,8 +2,12 @@ package priority
 
 import (
 	"context"
+	"math"
+	"math/rand"
+	"sync"
 	"testing"
 
+	"github.com/influxdata/tdigest"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -94,33 +98,250 @@ func TestLevelFromContext(t *testing.T) {
 	})
 }
 
-func TestLevelTracker_RecordLevel(t *testing.T) {
-	// Given
-	tracker := NewLevelTracker()
-	assert.Equal(t, 0.0, tracker.GetLevel(0.5))
+func TestWindowedLevelTracker_Empty(t *testing.T) {
+	tracker := NewLevelTracker(1000)
 
-	// When
-	tracker.RecordLevel(250)
-
-	// Then
-	assert.Equal(t, 250.0, tracker.GetLevel(0.5))
+	// Empty tracker should return 0
+	assert.Equal(t, 0, tracker.GetLevel(0.5))
+	assert.Equal(t, 0, tracker.GetLevel(0.0))
+	assert.Equal(t, 0, tracker.GetLevel(1.0))
 }
 
-func TestLevelTracker_GetLevel(t *testing.T) {
-	tracker := NewLevelTracker()
+func TestWindowedLevelTracker_SingleValue(t *testing.T) {
+	tracker := NewLevelTracker(1000)
 
-	// Given
-	levels := []int{100, 200, 300, 400}
+	tracker.RecordLevel(250)
+
+	// All quantiles should return the only value
+	assert.Equal(t, 250, tracker.GetLevel(0.0))
+	assert.Equal(t, 250, tracker.GetLevel(0.25))
+	assert.Equal(t, 250, tracker.GetLevel(0.5))
+	assert.Equal(t, 250, tracker.GetLevel(0.75))
+	assert.Equal(t, 250, tracker.GetLevel(1.0))
+}
+
+func TestWindowedLevelTracker_BasicQuantiles(t *testing.T) {
+	tracker := NewLevelTracker(1000)
+
+	// Record levels in order: 100, 200, 300, 400
+	levels := []int{400, 200, 100, 300}
 	for _, level := range levels {
 		tracker.RecordLevel(level)
 	}
 
-	// When
-	p25 := tracker.GetLevel(0.25)
-	p75 := tracker.GetLevel(0.75)
+	// Test quantiles
+	assert.Equal(t, 100, tracker.GetLevel(0.0))
+	assert.Equal(t, 100, tracker.GetLevel(0.25))
+	assert.Equal(t, 200, tracker.GetLevel(0.5))
+	assert.Equal(t, 300, tracker.GetLevel(0.75))
+	assert.Equal(t, 400, tracker.GetLevel(1.0))
+}
 
-	// Then
-	assert.True(t, p25 <= p75)
-	assert.True(t, p25 >= 100.0)
-	assert.True(t, p75 <= 400.0)
+func TestWindowedLevelTracker_UniformDistribution(t *testing.T) {
+	tracker := NewLevelTracker(100)
+
+	for i := 0; i < 100; i++ {
+		tracker.RecordLevel(i)
+	}
+
+	assert.Equal(t, 24, tracker.GetLevel(0.25))
+	assert.Equal(t, 49, tracker.GetLevel(0.50))
+	assert.Equal(t, 74, tracker.GetLevel(0.75))
+}
+
+func TestWindowedLevelTracker_SlidingWindow(t *testing.T) {
+	tracker := NewLevelTracker(3)
+
+	// Window should be [1, 2, 3]
+	tracker.RecordLevel(1)
+	tracker.RecordLevel(2)
+	tracker.RecordLevel(3)
+	assert.Equal(t, 2, tracker.GetLevel(0.5))
+
+	// Window should be [2, 3, 4]
+	tracker.RecordLevel(4)
+	assert.Equal(t, 3, tracker.GetLevel(0.5))
+
+	// Window should be [4, 5, 6]
+	tracker.RecordLevel(5)
+	tracker.RecordLevel(6)
+	assert.Equal(t, 5, tracker.GetLevel(0.5))
+}
+
+func TestWindowedLevelTracker_RejectionThreshold(t *testing.T) {
+	tracker := NewLevelTracker(100)
+	for i := 0; i < 70; i++ {
+		tracker.RecordLevel(i % 51) // levels 0-50
+	}
+	for i := 0; i < 20; i++ {
+		tracker.RecordLevel(51 + (i % 50)) // levels 51-100
+	}
+	for i := 0; i < 10; i++ {
+		tracker.RecordLevel(101 + (i % 50)) // levels 101-150
+	}
+
+	assert.True(t, tracker.GetLevel(0.3) <= 50)
+	assert.True(t, tracker.GetLevel(0.9) > 50)
+}
+
+func BenchmarkLevelTracker_Record(b *testing.B) {
+	b.Run("TDigest", func(b *testing.B) {
+		tracker := newTDigestLeveLTracker()
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			level := rand.Intn(499)
+			tracker.RecordLevel(level)
+		}
+	})
+
+	b.Run("Windowed", func(b *testing.B) {
+		tracker := NewLevelTracker(1000)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			level := rand.Intn(499)
+			tracker.RecordLevel(level)
+		}
+	})
+}
+
+func BenchmarkLevelTracker_GetLevel(b *testing.B) {
+	b.Run("TDigest", func(b *testing.B) {
+		tracker := newTDigestLeveLTracker()
+
+		for i := 0; i < 1000; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = tracker.GetLevel(0.3)
+		}
+	})
+
+	b.Run("Windowed", func(b *testing.B) {
+		tracker := NewLevelTracker(1000)
+
+		for i := 0; i < 1000; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = tracker.GetLevel(0.3)
+		}
+	})
+}
+
+func BenchmarkLevelTracker_RecordAndGet(b *testing.B) {
+	b.Run("TDigest", func(b *testing.B) {
+		tracker := newTDigestLeveLTracker()
+
+		for i := 0; i < 1000; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+			if i%100 == 0 {
+				_ = tracker.GetLevel(0.3)
+			}
+		}
+	})
+
+	b.Run("Windowed", func(b *testing.B) {
+		tracker := NewLevelTracker(1000)
+
+		for i := 0; i < 1000; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			tracker.RecordLevel(rand.Intn(499))
+			if i%100 == 0 {
+				_ = tracker.GetLevel(0.3)
+			}
+		}
+	})
+}
+
+func BenchmarkLevelTracker_RealisticWorkload(b *testing.B) {
+	priorityDist := func() int {
+		r := rand.Float64()
+		if r < 0.7 { // 70% high priority
+			return High.RandomLevel()
+		} else if r < 0.9 { // 20% medium priority
+			return Medium.RandomLevel()
+		} else { // 10% low priority
+			return Low.RandomLevel()
+		}
+	}
+
+	b.Run("TDigest", func(b *testing.B) {
+		tracker := newTDigestLeveLTracker()
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			tracker.RecordLevel(priorityDist())
+			if i%100 == 0 {
+				_ = tracker.GetLevel(0.3)
+			}
+		}
+	})
+
+	b.Run("Windowed", func(b *testing.B) {
+		tracker := NewLevelTracker(1000)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			tracker.RecordLevel(priorityDist())
+			if i%100 == 0 {
+				_ = tracker.GetLevel(0.3)
+			}
+		}
+	})
+}
+
+func BenchmarkLevelTracker_Memory(b *testing.B) {
+	b.Run("TDigest", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = newTDigestLeveLTracker()
+		}
+	})
+
+	b.Run("Windowed", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = NewLevelTracker(1000)
+		}
+	})
+}
+
+// tDigestLevelTracker provides a reference point for comparing the windowedLevelTracker.
+type tDigestLevelTracker struct {
+	mu     sync.Mutex
+	digest *tdigest.TDigest
+}
+
+func newTDigestLeveLTracker() LevelTracker {
+	return &tDigestLevelTracker{
+		digest: tdigest.NewWithCompression(100),
+	}
+}
+
+func (lt *tDigestLevelTracker) RecordLevel(level int) {
+	lt.mu.Lock()
+	defer lt.mu.Unlock()
+	lt.digest.Add(float64(level), 1.0)
+}
+
+func (lt *tDigestLevelTracker) GetLevel(quantile float64) int {
+	lt.mu.Lock()
+	defer lt.mu.Unlock()
+	if level := lt.digest.Quantile(quantile); !math.IsNaN(level) {
+		return int(level)
+	}
+	return 0
 }
